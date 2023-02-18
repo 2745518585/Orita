b19df8097b2569e6103ab1216b55b6bea15e4562
2745518585 <2745518585@qq.com>
2023-02-18 15:15:22 +0800

    Dev 0.0.8

--- compile.cpp
+++ compile.cpp
@@ -11,7 +11,15 @@ int compile_main(int argc,char **argv)
         for(int i=1;i<=get_sum_parameter("o");++i) compile_parameter=compile_parameter+" "+get_parameter("o",i);
     }
     if(!system("dir include"+system_to_nul)) compile_parameter+=" -I include";
-    if(get_sum_parameter("f")==-1)
+    if(get_sum_parameter("r")!=-1)
+    {
+        add_file(_compile_file,get_parameter("r",1),".cpp");
+        if(!compile(_compile_file,compile_parameter))
+        {
+            system(get_address(_compile_file)+"\\"+get_name_pre(_compile_file)+".exe");
+        }
+    }
+    else if(get_sum_parameter("f")==-1)
     {
         system("dir /b /s > files.txt");
         ifstream file(UTF8toGB("files.txt"));
@@ -23,7 +31,7 @@ int compile_main(int argc,char **argv)
             if(get_name_suf(_compile_file)==".cpp"&&get_name(_compile_file)!="compile.cpp")
             {
                 cout<<UTF8toGB(get_name(_compile_file))<<":\n";
-                if(compile(_compile_file,compile_parameter)==0) print_result(_Success);
+                if(!compile(_compile_file,compile_parameter)) print_result(_Success);
                 else print_result(_CE);
             }
         }
