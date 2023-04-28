2c3a132dee951e12312398bb81503400ec667fb2
2745518585 <2745518585@qq.com>
2023-04-28 18:50:15 +0800

    Dev 0.1.0: fix: linux print to nul

--- source/reset_Orita.cpp
+++ source/reset_Orita.cpp
@@ -12,27 +12,27 @@ std::string change_form(std::string str)
 int main()
 {
     #ifdef _WIN32
-    ssystem("rmdir /s /Q \""+appdata_path+"\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+"\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"data\" > nul 2>&1");
+    ssystem("rmdir /s /Q \""+appdata_path+"\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+"\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"data\""+system_to_nul);
     ssystem("echo 0 > \""+appdata_path+sPATH_SE+"data"+sPATH_SE+"data.in\"");
     ssystem("echo 0 > \""+appdata_path+sPATH_SE+"data"+sPATH_SE+"data.out\"");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"source\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"temp\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"random\" > nul 2>&1");
-    ssystem("copy \""+file_path+sPATH_SE+"files"+sPATH_SE+"*\" \""+appdata_path+"\" > nul 2>&1");
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"source\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"temp\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"random\""+system_to_nul);
+    ssystem("copy \""+file_path+sPATH_SE+"files"+sPATH_SE+"*\" \""+appdata_path+"\""+system_to_nul);
     ssystem("echo "+change_form("{\"name100\":\""+appdata_path+sPATH_SE+"0.cpp\",\"name101\":\""+appdata_path+sPATH_SE+"1.cpp\"}")+" > \""+appdata_path+sPATH_SE+"name.json\"");
     #endif
     #ifdef __linux__
-    ssystem("rm -r \""+appdata_path+"\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+"\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"data\" > nul 2>&1");
+    ssystem("rm -r \""+appdata_path+"\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+"\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"data\""+system_to_nul);
     ssystem("echo 0 > \""+appdata_path+sPATH_SE+"data"+sPATH_SE+"data.in\"");
     ssystem("echo 0 > \""+appdata_path+sPATH_SE+"data"+sPATH_SE+"data.out\"");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"source\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"temp\" > nul 2>&1");
-    ssystem("mkdir \""+appdata_path+sPATH_SE+"random\" > nul 2>&1");
-    ssystem("cp \""+file_path+sPATH_SE+"files"+sPATH_SE+"\"* \""+appdata_path+"\" > nul 2>&1");
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"source\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"temp\""+system_to_nul);
+    ssystem("mkdir \""+appdata_path+sPATH_SE+"random\""+system_to_nul);
+    ssystem("cp \""+file_path+sPATH_SE+"files"+sPATH_SE+"\"* \""+appdata_path+"\""+system_to_nul);
     ssystem("echo \\{\\\"name100\\\":\\\""+appdata_path+"/0.cpp\\\",\\\"name101\\\":\\\""+appdata_path+"/1.cpp\\\"\\} > \""+appdata_path+sPATH_SE+"name.json\"");
     #endif
     return 0;
