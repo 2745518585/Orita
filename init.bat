44796732a2126764c3ad63d1086083af71de148f
2745518585 <2745518585@qq.com>
2023-09-04 18:52:42 +0800

    update bin

--- init.bat
+++ init.bat
@@ -1,3 +1,3 @@
 mkdir %APPDATA%\Orita
 echo %~dp0build > %APPDATA%\Orita\path.txt 
-g++ bin\orita.cpp -o bin\orita.exe -std=c++20 -O2 -w
+g++ orita.cpp -o bin\orita.exe -std=c++20 -O2 -w
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 mkdir $HOME/.Orita
 echo $(cd $(dirname $0); pwd)/build > $HOME/.Orita/path.txt
-g++ bin/orita.cpp -o bin/orita -std=c++20 -O2 -I ${dir}/include -w;
+g++ orita.cpp -o bin/orita -std=c++20 -O2 -I ${dir}/include -w;
