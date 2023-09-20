7086ee0a2c7a408db98b51c6166f0e633ba568eb
2745518585 <2745518585@qq.com>
2023-09-20 14:14:47 +0800

    fix: mkdir bin

--- init.bat
+++ init.bat
@@ -1,3 +1,4 @@
 mkdir %APPDATA%\Orita
 echo %~dp0build > %APPDATA%\Orita\path.txt 
+mkdir bin
 g++ orita.cpp -o bin\orita.exe -std=c++20 -O2 -w
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 mkdir $HOME/.Orita
 echo $(cd $(dirname $0); pwd)/build > $HOME/.Orita/path.txt
+mkdir bin
 g++ orita.cpp -o bin/orita -std=c++20 -O2 -I ${dir}/include -w;
