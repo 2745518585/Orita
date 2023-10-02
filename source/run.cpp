2a108a2c964dfe7c38189f4e9804527c3153df5c
2745518585 <2745518585@qq.com>
2023-10-02 15:52:28 +0800

    print copying

--- source/run.cpp
+++ source/run.cpp
@@ -54,8 +54,8 @@ class Command_run: public App
         if(ans==fil()||!ans.exists()) {print_result(_ans_name,res::type::NF);return EXIT_NOINPUT;}
         if(chk==fil()||!chk.exists()) {print_result(_chk_name,res::type::NF);return EXIT_NOINPUT;}
         // compile file
-        printer loading_printer({"Compiling.","Compiling..","Compiling..."},(tim)150);
-        loading_printer.start();
+        printer *print=new printer({"Compiling.","Compiling..","Compiling..."},(tim)150);
+        print->start();
         compiler *run_compiler=new compiler(2);
         run_compiler->add({{_ans_name,ans},{_chk_name,chk}},compile_argu+" -D JUDGING");
         run_compiler->wait({_ans_name,_chk_name});
@@ -63,13 +63,13 @@ class Command_run: public App
             auto compile_result=run_compiler->get({_ans_name,_chk_name});
             if(compile_result.first)
             {
-                loading_printer.stop();
+                delete print;
                 print_result(compile_result.second,res::type::CE);
                 return EXIT_OK;
             }
         }
         delete run_compiler;
-        loading_printer.stop();
+        delete print;
         // run
         judger run_judger(ans,chk,in_file,out_file,ans_file,chk_file);
         run_judger.judge();
@@ -85,6 +85,8 @@ class Command_run: public App
         for(int j=1;j<=50;++j) output_chk_file<<"*";
         output_chk_file.close();
         // copy result
+        print=new printer({"Copying.","Copying..","Copying..."},(tim)150);
+        print->start();
         try
         {
             default_data_dir.createDirectory();
@@ -95,6 +97,7 @@ class Command_run: public App
             INFO("copy result",add_squo(default_data_dir.path()));
         }
         catch(...) {WARN("copy result - fail",add_squo(default_data_dir.path()));}
+        delete print;
         return EXIT_OK;
     }
 };
